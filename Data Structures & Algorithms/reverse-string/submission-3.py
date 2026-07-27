54d805a2034c04127a4452699fe6610488c019cc
Ching-Hsun (Hank), Tseng <48938318+hanktseng131415go@users.noreply.github.com>
2026-07-27 20:53:15 +0800

    Add: reverse-string - submission-3

---
+++ Data Structures & Algorithms/reverse-string/submission-3.py
@@ -0,0 +1,47 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        def two_pointers():
+            # time = n
+            # space = 1
+            l, r = 0, len(s)-1
+            while l <= r:
+                tmp = s[l]
+                s[l] = s[r]
+                s[r] = tmp
+                l+=1
+                r-=1
+        
+        two_pointers()
+
+        # s.reverse()
+        # # time = n
+        # # space = 1
+
+        # def stack():
+        #     # time: n
+        #     # space: n
+        #     array = []
+        #     for s_i in s:
+        #         array.append(s_i)
+            
+        #     i = 0
+        #     while array:
+        #         s[i] = array.pop()
+        #         i+=1
+        
+        # stack()
+
+        # def recursion(l, r):
+        #     # time = n
+        #     # space = n
+        #     if l <= r:
+        #         s[l], s[r] = s[r], s[l]
+        #         recursion(l+1, r-1)
+        
+        # recursion(0, len(s)-1)
+
+
+
