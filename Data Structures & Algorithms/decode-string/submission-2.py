bcdca49a4793f69b06387c751f307747478f041e
Ching-Hsun (Hank), Tseng <48938318+hanktseng131415go@users.noreply.github.com>
2026-08-25 20:42:41 +0800

    Add: decode-string - submission-2

---
+++ Data Structures & Algorithms/decode-string/submission-2.py
@@ -0,0 +1,46 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        
+        # two stack
+        # time: n+N
+        # space: n+N
+        s_stack, c_stack = [], []
+        cur, k = '', 0
+        for i in s:
+            if i.isdigit():
+                k = k * 10 + int(i)
+            elif i == '[':
+                s_stack.append(cur)
+                cur = ''
+                c_stack.append(k)
+                k = 0
+            elif i == ']':
+                tmp = cur
+                cur = s_stack.pop()
+                count = c_stack.pop()
+                cur += count * tmp
+            else:
+                cur += i
+        
+        return cur
+
+        # # one stack
+        # # time: n + N**2
+        # # space: n + N
+        # stack = []
+        # for i in s:
+        #     if i != ']':
+        #         stack.append(i)
+        #     else:
+        #         tmp = ''
+        #         while stack[-1] != '[':
+        #             tmp = stack.pop() + tmp
+        #         stack.pop()
+                
+        #         k = ''
+        #         while stack and stack[-1].isdigit():
+        #             k = stack.pop() + k
+                
+        #         stack.append(int(k) * tmp)
+        
+        # return ''.join(stack)
