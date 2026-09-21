d110ff5f98ea5ffae970b93556918e05c16e4ac5
Ching-Hsun (Hank), Tseng <48938318+hanktseng131415go@users.noreply.github.com>
2026-09-21 21:19:12 +0800

    Add: find-duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-7.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        # Negative Marking
+        for n in nums:
+            index = abs(n) - 1
+            if nums[index] < 0:
+                return abs(n)
+            
+            nums[index] *= -1
+        
+        return -1
+
+        # floyd's cycle linked list
+        # time: n
+        # space: 1
+        # slow, fast = 0, 0
+        # while True:
+        #     slow = nums[slow]
+        #     fast = nums[nums[fast]]
+        #     if slow == fast:
+        #         break
+        
+        # slow2 = 0
+        # while True:
+        #     slow = nums[slow]
+        #     slow2 = nums[slow2]
+        #     if slow == slow2:
+        #         return slow
